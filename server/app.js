3bdca020880ba78f572e1996b0fcffecb29b78fa
Asm Shad (INT) <asm.shad@liz.com>
2025-02-25 20:11:51 +0600

    MongoDB Connected

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+require("./connection/conn");
 
-app.get("/", (req,res) => {
-    res.send("Hello")
-})
-
-app.listen(1000, () => {
-  console.log("Server Started");
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
 
+app.listen(`${process.env.PORT}`, () => {
+  console.log("Server Started AT PORT = ${process.env.PORT}");
+});

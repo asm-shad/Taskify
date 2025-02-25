5bd394e9eec7ce3bfa5d3b945a6f354cf5023dfb
Asm Shad (INT) <asm.shad@liz.com>
2025-02-25 23:10:14 +0600

    Frontend Setup Complete

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-orange-600">
+      <h2 className="text-9xl">ASM</h2>
+    </div>
+  );
+};
+
+export default App;

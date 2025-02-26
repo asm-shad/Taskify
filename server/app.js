918f97e6ef801a91616bfb72504582356c23cf40
Asm Shad (INT) <asm.shad@liz.com>
2025-02-26 18:59:24 +0600

    Register Page connected with backened, cors handled and data posted via axios

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 require("./connection/conn");
 const userApis = require("./controllers/user");
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello");
